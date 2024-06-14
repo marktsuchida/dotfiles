d983b05c71431405ddbaf6f502161605e6f204fc
Mark A. Tsuchida <marktsuchida@gmail.com>
2024-06-14 15:45:18 -0500

    .., ..., etc., to cd to parent, for PowerShell

--- Documents/PowerShell/Profile.ps1
+++ Documents/PowerShell/Profile.ps1
@@ -5,3 +5,13 @@ if (-not ($Env:Path.Split(';') -contains "$Env:USERPROFILE\bin")) {
 }
 
 $Env:PIP_REQUIRE_VIRTUALENV = "true"
+
+function .. { Set-Location '..' }
+function ... { Set-Location '..\..' }
+function .... { Set-Location '..\..\..' }
+function ..... { Set-Location '..\..\..\..' }
+function ...... { Set-Location '..\..\..\..\..' }
+function ....... { Set-Location '..\..\..\..\..\..' }
+function ........ { Set-Location '..\..\..\..\..\..\..' }
+function ......... { Set-Location '..\..\..\..\..\..\..\..' }
+function .......... { Set-Location '..\..\..\..\..\..\..\..\..' }
