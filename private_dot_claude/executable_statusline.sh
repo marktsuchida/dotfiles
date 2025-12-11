83209e53cf6bbd96fec577fa18dc98868cecc419
Mark A. Tsuchida <marktsuchida@gmail.com>
2025-12-10 23:58:16 -0600

    Claude Code status line: show context % and model

--- private_dot_claude/executable_statusline.sh
+++ private_dot_claude/executable_statusline.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-pwd | sed "s|^$HOME|~|"
+input=$(cat)
 
-# This needs to be in a script due to claude bug on Windows that prevents
-# substitution of $HOME (as of this writing). Configure as follows:
-#
-#  "statusLine": {
-#    "type": "command",
-#    "command": "bash -c ~/.claude/statusline.sh"
-#  }
+INPUT_TOKENS=$(echo "$input" | jq -r '.context_window.total_input_tokens')
+OUTPUT_TOKENS=$(echo "$input" | jq -r '.context_window.total_output_tokens')
+CONTEXT_SIZE=$(echo "$input" | jq -r '.context_window.context_window_size')
+MODEL=$(echo "$input" | jq -r '.model.display_name')
+
+# This won't include system prompt and other front matter.
+TOTAL_TOKENS=$((INPUT_TOKENS + OUTPUT_TOKENS))
+PERCENT=$(echo "scale=1; $TOTAL_TOKENS * 100 / $CONTEXT_SIZE" | bc)
+
+echo "${PERCENT}% ${MODEL} $(pwd | sed "s|^$HOME|~|")"
