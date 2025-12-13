999f440229a91466e61d14dd1600372095b1abd4
Mark A. Tsuchida <marktsuchida@gmail.com>
2025-12-12 19:11:13 -0600

    Claude: remove attempt at token percent
    It doesn't work that way: total_input_tokens and total_output_tokens do
    not add up to a meaningful fraction of the context window (not clear to
    me what they actually represent). Remove for now.

--- private_dot_claude/executable_statusline.sh
+++ private_dot_claude/executable_statusline.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 input=$(cat)
 
-INPUT_TOKENS=$(echo "$input" | jq -r '.context_window.total_input_tokens')
-OUTPUT_TOKENS=$(echo "$input" | jq -r '.context_window.total_output_tokens')
-CONTEXT_SIZE=$(echo "$input" | jq -r '.context_window.context_window_size')
 MODEL=$(echo "$input" | jq -r '.model.display_name')
 
-# This won't include system prompt and other front matter.
-TOTAL_TOKENS=$((INPUT_TOKENS + OUTPUT_TOKENS))
-PERCENT=$(echo "scale=1; $TOTAL_TOKENS * 100 / $CONTEXT_SIZE" | bc)
-
-echo "${PERCENT}% ${MODEL} $(pwd | sed "s|^$HOME|~|")"
+echo "${MODEL} $(pwd | sed "s|^$HOME|~|")"
